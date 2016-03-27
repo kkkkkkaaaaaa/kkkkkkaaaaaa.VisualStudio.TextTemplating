9cbe68cbf309a4ae55e083e432a0131a49275041
kkkkkkaaaaaa <kkkkkkaaaaaa@hotmail.co.jp>
2016-03-27 14:50:05 +0900

    Update kkkkkkaaaaaa.VisualStudio.TextTemplating Projects.

---
+++ TextTemplating.Xunit/NotifyPropertyChangedTemplateFacts.cs
@@ -0,0 +1,29 @@
+﻿using kkkkkkaaaaaa.VisualStudio.TextTemplating;
+using kkkkkkaaaaaa.VisualStudio.TextTemplating.DataTransferObjects;
+using Xunit;
+
+namespace kkkkkkaaaaaa.VIsualStudio.TextTemplating.Xunit
+{
+    public class NotifyPropertyChangedTemplateFacts
+    {
+        [Fact()]
+        public void Fact()
+        {
+            var entity  = new EntitiesContext()
+            {
+                Namespace = @"Namespace",
+                Imports = new [] { @"Import", },
+                TypeName = @"TypeName",
+                TypeNameSuffix = @"Suffix",
+                Inherits = @"BaseClass",
+                Columns = new []
+                {
+                    new SqlColumnsSchemaEntity() { ColumnName = @"ColumnName", ColumnSize = 1, DataType = typeof(string), DataTypeName = @"string", NumericScale = 7 },
+                }
+            };
+
+            var template = new NotifyPropertyChangedTemplate(entity);
+            var text = template.TransformText();
+        }
+    }
+}
